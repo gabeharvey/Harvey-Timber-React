63b83470251dab3af3dbc121f65a497bf0e33dc4
Gabe Harvey <harvey7@gmail.com>
2024-08-03 11:57:48 -0500

    Added Navbar import to App.jsx

--- client/src/utils/App.jsx
+++ client/src/utils/App.jsx
@@ -2,6 +2,7 @@ import {Outlet} from 'react-router-dom';
 import '../utils/App.css'
 import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
+import Navbar from '/src/components/Navbar.jsx';
 
 const httpLink = createHttpLink ({
     uri: '/graphql',
