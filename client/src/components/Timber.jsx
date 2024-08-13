b20ad7f5af4a57f0bb1e9b4c69b37020081429d5
Gabe Harvey <harvey7@gmail.com>
2024-08-13 11:43:09 -0500

    Added Timber.jsx styling and features

---
+++ client/src/components/Timber.jsx
@@ -0,0 +1,49 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+const Timber = () => {
+  const images = [
+    { src: '/src/assets/timber1.jpg', label: 'Timber View 1' },
+    { src: '/src/assets/timber2.jpg', label: 'Timber View 2' },
+    { src: '/src/assets/timber3.jpg', label: 'Timber View 3' },
+    { src: '/src/assets/timber4.jpg', label: 'Timber View 4' },
+  ];
+
+  return (
+    <Flex direction="column" align="center" justify="center" minHeight="100vh" bg='#45503B' p={5} textAlign="center">
+      <Text fontSize="3xl" fontFamily='Rakkas, cursive' color='white' mb={10}>
+        Timber Gallery
+      </Text>
+      <Flex direction={['column', 'row']} wrap="wrap" justify="center">
+        {images.map((image, index) => (
+          <Box
+            key={index}
+            bgImage={`url(${image.src})`}
+            bgSize="cover"
+            bgPos="center"
+            boxShadow="md"
+            p={4}
+            rounded="md"
+            m={4}
+            width="300px"
+            height="200px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" fontFamily='Rakkas, cursive' color='white'>
+              {image.label}
+            </Text>
+          </Box>
+        ))}
+      </Flex>
+      <Link to='/'>
+        <Text fontSize="xl" fontFamily='Rakkas, cursive' color='white' mt={10} textDecoration="underline">
+          Back to Home
+        </Text>
+      </Link>
+    </Flex>
+  );
+};
+
+export default Timber;
