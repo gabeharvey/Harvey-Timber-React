cef458d2f1ab6a465f83165dc5328a051d4f2f62
Gabe Harvey <harvey7@gmail.com>
2024-08-30 12:42:19 -0500

    Minor code cleanup

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -6,17 +6,12 @@ import PropTypes from 'prop-types';
 const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
 
-  // Check if the user is authenticated
   if (isAuthenticated) {
-    // If user is authenticated and trying to access login or signup pages, redirect to home
     if (window.location.pathname === '/login' || window.location.pathname === '/signup') {
       return <Navigate to="/" />;
     }
-    // If user is authenticated and trying to access a private route, allow access
     return children;
   }
-
-  // If user is not authenticated, redirect to login page
   return <Navigate to="/login" />;
 };
 
