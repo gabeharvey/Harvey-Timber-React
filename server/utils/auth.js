279a1c463597e8b06973f4f47af93e612f9cedcf
Gabe Harvey <harvey7@gmail.com>
2024-08-29 19:03:19 -0500

    Added auth.js file

---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { authenticatedUser } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = authenticatedUser;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ authenticatedUser: payload }, secret, { expiresIn: expiration });
+  },
+};
