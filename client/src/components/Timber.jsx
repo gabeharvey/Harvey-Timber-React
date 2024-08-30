8c20771479d84850a0e05428c4248bdfc797d304
Gabe Harvey <harvey7@gmail.com>
2024-08-30 11:51:24 -0500

    Updated Timber.jsx to add carousel

--- client/src/components/Timber.jsx
+++ client/src/components/Timber.jsx
@@ -1,18 +1,20 @@
 import { Box, Flex, Text, Button } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import quotes from '../utils/quotes.json'
-import {useState, useEffect} from 'react'
+import quotes from '../utils/quotes.json';
+import { useState, useEffect } from 'react';
+import Slider from 'react-slick';
 
 const Timber = () => {
-    const [randomQuote, setRandomQuote] = useState ('');
-    useEffect (() => {
-        const getRandomQuote = () => {
-            const randomIndex = Math.floor(Math.random () * quotes.length);
-            return quotes[randomIndex];
-        };
-        setRandomQuote (getRandomQuote ());
-    }, []);
- 
+  const [randomQuote, setRandomQuote] = useState('');
+
+  useEffect(() => {
+    const getRandomQuote = () => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      return quotes[randomIndex];
+    };
+    setRandomQuote(getRandomQuote());
+  }, []);
+
   const images = [
     { src: '/src/assets/timber-image-0.jpg', label: 'Timber View 0' },
     { src: '/src/assets/timber-image-1.jpg', label: 'Timber View 1' },
@@ -26,36 +28,45 @@ const Timber = () => {
     { src: '/src/assets/timber-image-9.jpg', label: 'Timber View 9' }
   ];
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   return (
     <Flex direction="column" align="center" justify="center" minHeight="100vh" bg='#45503B' p={5} textAlign="center">
       <Text fontSize="2xl" fontFamily='Chakra Petch, cursive' fontWeight='bold' color='white' mb={10}>
         Timber Gallery
       </Text>
-      <Flex direction={['column', 'row']} wrap="wrap" justify="center">
-        {images.map((image, index) => (
-          <Box
-            key={index}
-            bgImage={`url(${image.src})`}
-            bgSize="cover"
-            bgPos="center"
-            boxShadow="black 5px 5px 5px"
-            p={4}
-            rounded="md"
-            m={4}
-            width="300px"
-            height="400px"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text fontSize="xl" fontFamily='Chakra Petch, cursive' color='transparent'>
-              {image.label}
-            </Text>
-          </Box>
-        ))}
-      </Flex>
+      <Box width="40%" mb={10}>
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <Box
+              key={index}
+              bgImage={`url(${image.src})`}
+              bgSize="cover"
+              bgPos="center"
+              
+              p={4}
+              rounded="lg"
+              height="400px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text fontSize="xl" fontFamily='Chakra Petch, cursive' color='transparent'>
+                {image.label}
+              </Text>
+            </Box>
+          ))}
+        </Slider>
+      </Box>
       <Link to='/'>
-      <Button
+        <Button
           mt={10}
           bg='gray'
           color='white'
@@ -69,7 +80,7 @@ const Timber = () => {
         </Button>
       </Link>
       <Text fontSize='16px' fontFamily="Chakra Petch, cursive" fontWeight='bold' color='white' maxW="90%" mb="6" mt="10">
-            {randomQuote.quote}
+        {randomQuote.quote}
       </Text>
     </Flex>
   );
